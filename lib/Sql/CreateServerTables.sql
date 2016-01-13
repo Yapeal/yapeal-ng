a387b73f4370c8bf5caefbb30322f0f4b3ef1c32
Michael Cummings <mgcummings@yahoo.com>
2016-01-13 05:25:27 -0800

    Removed old per section Create*Tables.sql to make room for per API ones.

--- lib/Sql/CreateServerTables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "{database}"."{table_prefix}serverServerStatus" (
-    "onlinePlayers" BIGINT(20) UNSIGNED NOT NULL,
-    "serverName"    CHAR(24)            NOT NULL,
-    "serverOpen"    CHAR(5)             NOT NULL,
-    PRIMARY KEY ("serverName")
-)
-ENGINE = { engine}
-DEFAULT CHARSET = utf8mb4
-COLLATE = utf8mb4_unicode_520_ci;
