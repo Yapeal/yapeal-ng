dab1ef20480031bf7cf78f8979b2cea5f3af22b5
Michael Cummings <mgcummings@yahoo.com>
2016-03-03 22:04:21 -0800

    Removed incorrect/incomplete SkillInTraining and ServerStatus stuff.

--- lib/Sql/Char/CreateSkillInTraining.sql
+++
@@ -1,8 +0,0 @@
--- Sql/Char/CreateSkillInTraining.sql
--- version 20160201053950.120
-START TRANSACTION;
-INSERT INTO "{database}"."{table_prefix}utilDatabaseVersion" ("version")
-VALUES
-('20160201053950.120')
-ON DUPLICATE KEY UPDATE "version" = VALUES("version");
-COMMIT;
--- lib/Sql/Server/CreateServerStatus.sql
+++
@@ -1,8 +0,0 @@
--- Sql/Server/CreateServerStatus.sql
--- version 20160201053949.169
-START TRANSACTION;
-INSERT INTO "{database}"."{table_prefix}utilDatabaseVersion" ("version")
-VALUES
-('20160201053949.169')
-ON DUPLICATE KEY UPDATE "version" = VALUES("version");
-COMMIT;
