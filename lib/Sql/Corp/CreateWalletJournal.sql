00573396daa5827dbd9236e5d92cba0235168df3
Michael Cummings <mgcummings@yahoo.com>
2016-03-16 20:17:36 -0700

    Updated lib/Sql/{Char,Corp}/CreateWallet*.sql to have accountKey column.

--- lib/Sql/Corp/CreateWalletJournal.sql
+++ lib/Sql/Corp/CreateWalletJournal.sql
@@ -1,21 +1,22 @@
 -- Sql/Corp/CreateWalletJournal.sql
 -- version 20160201053952.478
 CREATE TABLE "{database}"."{table_prefix}corpWalletJournal" (
-    "amount"       DECIMAL(17, 2)      NOT NULL,
-    "argID1"       BIGINT(20) UNSIGNED NOT NULL,
-    "argName1"     CHAR(100)           NOT NULL,
-    "balance"      DECIMAL(17, 2)      NOT NULL,
-    "date"         DATETIME            NOT NULL,
-    "owner1TypeID" BIGINT(20) UNSIGNED NOT NULL,
-    "owner2TypeID" BIGINT(20) UNSIGNED NOT NULL,
-    "ownerID"      BIGINT(20) UNSIGNED NOT NULL,
-    "ownerID1"     BIGINT(20) UNSIGNED NOT NULL,
-    "ownerID2"     BIGINT(20) UNSIGNED NOT NULL,
-    "ownerName1"   CHAR(100)           NOT NULL,
-    "ownerName2"   CHAR(100)           NOT NULL,
+    "accountKey"   SMALLINT(5) UNSIGNED NOT NULL,
+    "amount"       DECIMAL(17, 2)       NOT NULL,
+    "argID1"       BIGINT(20) UNSIGNED  NOT NULL,
+    "argName1"     CHAR(100)            NOT NULL,
+    "balance"      DECIMAL(17, 2)       NOT NULL,
+    "date"         DATETIME             NOT NULL,
+    "owner1TypeID" BIGINT(20) UNSIGNED  NOT NULL,
+    "owner2TypeID" BIGINT(20) UNSIGNED  NOT NULL,
+    "ownerID"      BIGINT(20) UNSIGNED  NOT NULL,
+    "ownerID1"     BIGINT(20) UNSIGNED  NOT NULL,
+    "ownerID2"     BIGINT(20) UNSIGNED  NOT NULL,
+    "ownerName1"   CHAR(100)            NOT NULL,
+    "ownerName2"   CHAR(100)            NOT NULL,
     "reason"       TEXT,
-    "refID"        BIGINT(20) UNSIGNED NOT NULL,
-    "refTypeID"    BIGINT(20) UNSIGNED NOT NULL,
+    "refID"        BIGINT(20) UNSIGNED  NOT NULL,
+    "refTypeID"    BIGINT(20) UNSIGNED  NOT NULL,
     PRIMARY KEY ("ownerID", "refID")
 );
 START TRANSACTION;
