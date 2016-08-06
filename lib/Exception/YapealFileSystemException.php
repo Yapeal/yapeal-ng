a697c3c0ec9064e3cf005ff2089e055b606c4b36
Michael Cummings <mgcummings@yahoo.com>
2016-08-05 18:16:07 -0700

    Add back an old Yapeal FileSystem Exception for internally use only.

---
+++ lib/Exception/YapealFileSystemException.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class YapealFileSystemException.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0+
+ */
+namespace Yapeal\Exception;
+
+/**
+ * Class YapealFileSystemException.
+ *
+ * This exception is expected to be used internally only and must be caught
+ * inside Yapeal-ng itself some where.
+ */
+class YapealFileSystemException extends YapealException
+{
+}
