ecf2c195accf0372bd892c0a2db450652c16d46f
Michael Cummings <mgcummings@yahoo.com>
2016-08-10 04:01:51 -0700

    Renamed Sql/Database to Sql/Schema. Changed name of Sql/Schema/*.sql files to reflect directory name change etc as well.

--- lib/Sql/Database/DropDatabase.mysql.sql
+++
@@ -1,3 +0,0 @@
--- Sql/Database/DropDatabase.sql
--- version 20160627181942.283
-DROP DATABASE IF EXISTS "{database}";
---
+++ lib/Sql/Schema/DropSchema.mysql.sql
@@ -0,0 +1,3 @@
+-- Sql/Schema/DropDatabase.sql
+-- version 20160627181942.283
+DROP SCHEMA IF EXISTS "{schema}";
