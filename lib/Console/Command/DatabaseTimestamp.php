6df30489987b76e2d776b10c68b621d17b1727bb
Michael Cummings <mgcummings@yahoo.com>
2016-08-10 03:47:49 -0700

    Added new console command Database:Timestamp so don't have to re-create manually on CLI all the time when needed.

---
+++ lib/Console/Command/DatabaseTimestamp.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class DatabaseTimestamp.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0+
+ */
+namespace Yapeal\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class DatabaseTimestamp.
+ */
+class DatabaseTimestamp extends Command
+{
+    /**
+     * Constructor.
+     *
+     * @param string|null $name The name of the command; passing null means it must be set in configure()
+     *
+     * @throws \LogicException When the command name is empty
+     */
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+    }
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Executes the current command.
+     *
+     * This method is not abstract because you can use this class
+     * as a concrete class. In this case, instead of defining the
+     * execute() method, you set the code to execute by passing
+     * a Closure to the setCode() method.
+     *
+     * @param InputInterface  $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return null|int null or 0 if everything went fine, or an error code
+     *
+     * @see setCode()
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        list($mSec, $sec) = explode(' ', microtime());
+        $output->writeln(gmdate('YmdHis', (int)$sec) . substr($mSec, 1, 4));
+        return 0;
+    }
+}
