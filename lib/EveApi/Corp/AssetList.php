fa90b8ff3b7f01a833e1dbf74299c08c52b93cf0
Michael Cummings <mgcummings@yahoo.com>
2016-08-10 20:27:34 -0700

    Adding in last parts of AssetList for Char and Corp should be working now.

---
+++ lib/EveApi/Corp/AssetList.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class AssetList.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0+
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ */
+namespace Yapeal\EveApi\Corp;
+
+use Yapeal\EveApi\NestedSetTrait;
+use Yapeal\Log\Logger;
+use Yapeal\Sql\PreserverTrait;
+use Yapeal\Xml\EveApiReadWriteInterface;
+
+/**
+ * Class AssetList.
+ */
+class AssetList extends CorpSection
+{
+    use PreserverTrait, NestedSetTrait;
+
+    /** @noinspection MagicMethodsValidityInspection */
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->mask = 2;
+        $this->preserveTos = ['preserveToAssetList'];
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return self Fluent interface.
+     * @throws \LogicException
+     */
+    protected function preserveToAssetList(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'corpAssetList';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $this->getFilteredSqlMessage($sql));
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'flag' => '0',
+            'itemID' => null,
+            'lft' => null,
+            'lvl' => null,
+            'locationID' => null,
+            'ownerID' => $ownerID,
+            'quantity' => '1',
+            'rawQuantity' => '0',
+            'rgt' => null,
+            'singleton' => '0',
+            'typeID' => null
+        ];
+        $xPath = '//assets/row';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->attributePreserveData($elements, $columnDefaults, $tableName);
+        $simple = new \SimpleXMLElement($data->getEveApiXml());
+        /** @noinspection PhpUndefinedFieldInspection */
+        if (0 !== $simple->result[0]->count()) {
+            /** @noinspection PhpUndefinedFieldInspection */
+            $simple->result[0]->row[0]['itemID'] = $ownerID;
+            /** @noinspection PhpUndefinedFieldInspection */
+            $this->addNesting($simple->result[0]->row[0]);
+        }
+        return $this->attributePreserveData($simple->asXML(), $columnDefaults, $tableName);
+    }
+}
