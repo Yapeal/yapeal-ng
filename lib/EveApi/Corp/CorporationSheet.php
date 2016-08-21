e567f6c1fecaa17eeeb3e73a35cd331359da832e
Michael Cummings <mgcummings@yahoo.com>
2016-08-21 12:51:22 -0700

    Adding CorporationSheet API. Files *.{php, mysql.sql, xsd}

---
+++ lib/EveApi/Corp/CorporationSheet.php
@@ -0,0 +1,189 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class CorporationSheet.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0+
+ */
+namespace Yapeal\EveApi\Corp;
+
+use Yapeal\Log\Logger;
+use Yapeal\Sql\PreserverTrait;
+use Yapeal\Xml\EveApiReadWriteInterface;
+
+/**
+ * Class CorporationSheet.
+ */
+class CorporationSheet extends CorpSection
+{
+    use PreserverTrait;
+
+    /** @noinspection MagicMethodsValidityInspection */
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->mask = 8;
+        $this->preserveTos = [
+            'preserveToCorporationSheet',
+            'preserverToDivisions',
+            'preserverToLogo',
+            'preserverToWalletDivisions'
+        ];
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return void
+     * @throws \DomainException
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    protected function preserverToCorporationSheet(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'corpCorporationSheet';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $sql);
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'allianceID' => '0',
+            'allianceName' => '',
+            'ceoID' => null,
+            'ceoName' => null,
+            'corporationID' => $ownerID,
+            'corporationName' => null,
+            'description' => null,
+            'factionID' => '0',
+            'factionName' => '',
+            'memberCount' => null,
+            'memberLimit' => '0',
+            'shares' => null,
+            'stationID' => null,
+            'stationName' => null,
+            'taxRate' => null,
+            'ticker' => null,
+            'url' => null
+        ];
+        $xPath = '//result/child::*[not(*|@*|self::dataTime)]';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->valuesPreserveData($elements, $columnDefaults, $tableName);
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return void
+     * @throws \DomainException
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    protected function preserverToDivisions(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'corpDivisions';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $sql);
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'ownerID' => $ownerID,
+            'accountKey' => null,
+            'description' => null
+        ];
+        $xPath = '//divisions/row';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->attributePreserveData($elements, $columnDefaults, $tableName);
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return void
+     * @throws \DomainException
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    protected function preserverToLogo(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'corpLogo';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $sql);
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'ownerID' => $ownerID,
+            'color1' => null,
+            'color2' => null,
+            'color3' => null,
+            'graphicID' => null,
+            'shape1' => null,
+            'shape2' => null,
+            'shape3' => null
+        ];
+        $xPath = '//logo/*';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->valuesPreserveData($elements, $columnDefaults, $tableName);
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return void
+     * @throws \DomainException
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    protected function preserverToWalletDivisions(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'corpWalletDivisions';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $sql);
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'ownerID' => $ownerID,
+            'accountKey' => null,
+            'description' => null
+        ];
+        $xPath = '//walletDivisions/row';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->attributePreserveData($elements, $columnDefaults, $tableName);
+    }
+}
---
+++ lib/Sql/Corp/CreateCorporationSheet.mysql.sql
@@ -0,0 +1,52 @@
+-- Sql/Corp/CreateCorporationSheet.mysql.sql
+-- version 20160821184013.625
+-- noinspection SqlResolveForFile
+CREATE TABLE "{database}"."{tablePrefix}corpCorporationSheet" (
+    "allianceID"      BIGINT(20) UNSIGNED    NOT NULL,
+    "allianceName"    CHAR(100)              NOT NULL,
+    "ceoID"           BIGINT(20) UNSIGNED    NOT NULL,
+    "ceoName"         CHAR(100)              NOT NULL,
+    "corporationID"   BIGINT(20) UNSIGNED    NOT NULL,
+    "corporationName" CHAR(100)              NOT NULL,
+    "description"     TEXT,
+    "factionID"       BIGINT(20) UNSIGNED    NOT NULL DEFAULT 0,
+    "factionName"     CHAR(100)              NOT NULL DEFAULT '',
+    "memberCount"     BIGINT(20) UNSIGNED    NOT NULL,
+    "memberLimit"     BIGINT(20) UNSIGNED    NOT NULL DEFAULT 0,
+    "shares"          BIGINT(20) UNSIGNED    NOT NULL,
+    "stationID"       BIGINT(20) UNSIGNED    NOT NULL,
+    "stationName"     CHAR(100)              NOT NULL,
+    "taxRate"         DECIMAL(5, 2) UNSIGNED NOT NULL,
+    "ticker"          CHAR(5)                NOT NULL,
+    "url"             CHAR(255)              NOT NULL DEFAULT '',
+    PRIMARY KEY ("corporationID")
+);
+CREATE TABLE "{database}"."{tablePrefix}corpDivisions" (
+    "ownerID"     BIGINT(20) UNSIGNED  NOT NULL,
+    "accountKey"  SMALLINT(5) UNSIGNED NOT NULL,
+    "description" CHAR(255)            NOT NULL,
+    PRIMARY KEY ("ownerID", "accountKey")
+);
+CREATE TABLE "{database}"."{tablePrefix}corpLogo" (
+    "ownerID"   BIGINT(20) UNSIGNED  NOT NULL,
+    "color1"    SMALLINT(5) UNSIGNED NOT NULL,
+    "color2"    SMALLINT(5) UNSIGNED NOT NULL,
+    "color3"    SMALLINT(5) UNSIGNED NOT NULL,
+    "graphicID" BIGINT(20) UNSIGNED  NOT NULL,
+    "shape1"    SMALLINT(5) UNSIGNED NOT NULL,
+    "shape2"    SMALLINT(5) UNSIGNED NOT NULL,
+    "shape3"    SMALLINT(5) UNSIGNED NOT NULL,
+    PRIMARY KEY ("ownerID", "graphicID")
+);
+CREATE TABLE "{database}"."{tablePrefix}corpWalletDivisions" (
+    "ownerID"     BIGINT(20) UNSIGNED  NOT NULL,
+    "accountKey"  SMALLINT(4) UNSIGNED NOT NULL,
+    "description" CHAR(255)            NOT NULL,
+    PRIMARY KEY ("ownerID", "accountKey")
+);
+START TRANSACTION;
+-- @formatter:off
+INSERT INTO "{schema}"."{tablePrefix}utilDatabaseVersion" ("version")
+ VALUES ('20160821184013.625')
+ ON DUPLICATE KEY UPDATE "version" = VALUES("version");
+COMMIT;
