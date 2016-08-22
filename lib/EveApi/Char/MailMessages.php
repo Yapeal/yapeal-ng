9055bfb0a44aaa6738e6d475338d134719f0dec6
Michael Cummings <mgcummings@yahoo.com>
2016-08-22 11:35:53 -0700

    Added Everything for Char/MailMessages API.

---
+++ lib/EveApi/Char/MailMessages.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class MailMessages.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0+
+ */
+namespace Yapeal\EveApi\Char;
+
+use Yapeal\Log\Logger;
+use Yapeal\Sql\PreserverTrait;
+use Yapeal\Xml\EveApiReadWriteInterface;
+
+/**
+ * Class MailMessages.
+ */
+class MailMessages extends CharSection
+{
+    use PreserverTrait;
+
+    /** @noinspection MagicMethodsValidityInspection */
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->mask = 2048;
+        $this->preserveTos = [
+            'preserveToMailMessages'
+        ];
+    }
+    /**
+     * @param EveApiReadWriteInterface $data
+     *
+     * @return void
+     * @throws \LogicException
+     */
+    protected function preserveToMailMessages(EveApiReadWriteInterface $data)
+    {
+        $tableName = 'charMailMessages';
+        $ownerID = $this->extractOwnerID($data->getEveApiArguments());
+        $sql = $this->getCsq()
+            ->getDeleteFromTableWithOwnerID($tableName, $ownerID);
+        $this->getYem()
+            ->triggerLogEvent('Yapeal.Log.log', Logger::DEBUG, $sql);
+        $this->getPdo()
+            ->exec($sql);
+        $columnDefaults = [
+            'messageID' => null,
+            'ownerID' => $ownerID,
+            'senderID' => null,
+            'senderName' => null,
+            'senderTypeID' => null,
+            'sentDate' => null,
+            'title' => null,
+            'toCharacterIDs' => null,
+            'toCorpOrAllianceID' => '0',
+            'toListID' => null
+        ];
+        $xPath = '//messages/row';
+        $elements = (new \SimpleXMLElement($data->getEveApiXml()))->xpath($xPath);
+        $this->attributePreserveData($elements, $columnDefaults, $tableName);
+    }
+}
---
+++ lib/Sql/Char/CreateMailMessages.mysql.sql
@@ -0,0 +1,20 @@
+-- Sql/Char/CreateMailMessages.mysql.sql
+-- version 20160822175059.411
+CREATE TABLE "{database}"."{tablePrefix}charMailMessages" (
+    "messageID"          BIGINT(20) UNSIGNED NOT NULL,
+    "ownerID"            BIGINT(20) UNSIGNED NOT NULL,
+    "senderID"           BIGINT(20) UNSIGNED NOT NULL,
+    "senderName"         CHAR(100)           DEFAULT NULL,
+    "senderTypeID"       BIGINT(20) UNSIGNED DEFAULT NULL,
+    "sentDate"           DATETIME            NOT NULL,
+    "title"              CHAR(255)           DEFAULT NULL,
+    "toCharacterIDs"     TEXT,
+    "toCorpOrAllianceID" BIGINT(20) UNSIGNED DEFAULT '0',
+    "toListID"           TEXT,
+    PRIMARY KEY ("ownerID", "messageID")
+);
+-- @formatter:off
+INSERT INTO "{schema}"."{tablePrefix}utilDatabaseVersion" ("version")
+    VALUES ('20160822175059.411')
+    ON DUPLICATE KEY UPDATE "version" = VALUES("version");
+COMMIT;
