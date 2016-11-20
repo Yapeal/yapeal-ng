c1c8b584c676845d8ca69cf83062a6f88de390b2
Michael Cummings <mgcummings@yahoo.com>
2016-11-19 16:01:49 -0800

    Added Spec examples for Event/EveApiEvent and LogEvent classes.

---
+++ specs/Spec/Event/EveApiEventSpec.php
@@ -0,0 +1,103 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Contains class EveApiEventSpec.
+ *
+ * PHP version 7.0
+ *
+ * LICENSE:
+ * This file is part of Yet Another Php Eve Api Library also know as Yapeal
+ * which can be used to access the Eve Online API data and place it into a
+ * database.
+ * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see
+ * <http://spdx.org/licenses/LGPL-3.0.html>.
+ *
+ * You should be able to find a copy of this license in the COPYING-LESSER.md
+ * file. A copy of the GNU GPL should also be available in the COPYING.md file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2016 Michael Cummings
+ * @license   LGPL-3.0
+ */
+namespace Spec\Yapeal\Event;
+
+use PhpSpec\ObjectBehavior;
+
+/**
+ * Class EveApiEventSpec
+ *
+ * @mixin \Yapeal\Event\EveApiEvent
+ *
+ * @method void during($method, array $params)
+ * @method void shouldBe($value)
+ * @method void shouldContain($value)
+ * @method void shouldNotEqual($value)
+ * @method void shouldReturn($result)
+ */
+class EveApiEventSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Yapeal\Event\EveApiEvent');
+        $this->shouldImplement('Yapeal\Event\EveApiEventInterface');
+    }
+    /**
+     *
+     */
+    public function it_provides_fluent_interface_from_event_handled()
+    {
+        $this->eventHandled()
+            ->shouldReturn($this);
+    }
+    /**
+     *
+     */
+    public function it_provides_fluent_interface_from_event_sufficiently_handled()
+    {
+        $this->setHandledSufficiently()
+            ->shouldReturn($this);
+    }
+    /**
+     *
+     */
+    public function it_should_have_handled_event_after_event_handled_is_used()
+    {
+        $this->eventHandled()
+            ->shouldHaveBeenHandled();
+    }
+    /**
+     *
+     */
+    public function it_should_have_sufficiently_handled_event_after_sufficiently_handled_is_used()
+    {
+        $this->setHandledSufficiently()
+            ->shouldBeSufficientlyHandled();
+    }
+    /**
+     *
+     */
+    public function it_should_not_have_handled_event_initially()
+    {
+        $this->shouldNotHaveBeenHandled();
+    }
+    /**
+     *
+     */
+    public function it_should_not_have_sufficiently_handled_event_initially()
+    {
+        $this->shouldNotBeSufficientlyHandled();
+    }
+}
