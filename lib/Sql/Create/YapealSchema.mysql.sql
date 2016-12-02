aaaba87437d92b2af4bea33f84f3030f1a9156e1
Michael Cummings <mgcummings@yahoo.com>
2016-12-01 20:00:45 -0800

    Moved and renamed Sql/Queries/getCreateYapealEveApi.mysql.sql to Sql/Create/Yapeal/EveApi.mysql.sql. Added create yapealSchemaVersion table to Sql/Create/YapealSchema.mysql.sql so it's always available for the other create tables and update tables SQL files and should never end up with unknown schema version.

--- lib/Sql/Create/YapealSchema.mysql.sql
+++ lib/Sql/Create/YapealSchema.mysql.sql
@@ -3,3 +3,12 @@
 CREATE SCHEMA "{schema}"
     DEFAULT CHARACTER SET '{characterSet}'
     DEFAULT COLLATE '{characterCollate}';
+CREATE TABLE "{schema}"."{tablePrefix}yapealSchemaVersion" (
+    "version" CHAR(18) NOT NULL,
+    PRIMARY KEY ("version")
+);
+START TRANSACTION;
+-- @formatter:off
+INSERT INTO "{schema}"."{tablePrefix}yapealSchemaVersion" ("version")
+    VALUES ('20161129113301.000');
+COMMIT;
