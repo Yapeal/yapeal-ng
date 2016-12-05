cd69e71011c94e606115b0d5cd45e43ddc27706a
Michael Cummings <mgcummings@yahoo.com>
2016-12-05 03:41:53 -0800

    Added all the required context methods in features/bootstrap/AdminToolsContext.

--- features/bootstrap/AdminToolsContext.php
+++ features/bootstrap/AdminToolsContext.php
@@ -35,7 +35,6 @@ declare(strict_types = 1);
 namespace Yapeal\Behat;
 
 use Behat\Behat\Context\Context;
-use Behat\Behat\Tester\Exception\PendingException;
 use Webmozart\Assert\Assert;
 use Yapeal\AdminTools\ManageRegisteredKey;
 use Yapeal\Configuration\Wiring;
@@ -64,12 +63,15 @@ class AdminToolsContext implements Context
      */
     public function clearTestRowAfterScenario()
     {
-        try {
+        Assert::false($this->pdo->inTransaction(), 'Still have pending transaction');
+        if (is_array($this->key) && array_key_exists('keyID', $this->key)) {
             $sql = $this->csq->getDeleteFromTableWithKeyID('yapealRegisteredKey', $this->key['keyID']);
-            $this->pdo->exec($sql);
-        } catch (\Throwable $exc) {
-            // Ignore any problems nothing to do about them here.
+            Assert::true($this->pdo->beginTransaction(), 'Could not start transaction for test row');
+            $stmt = $this->pdo->prepare($sql);
+            Assert::true($stmt->execute(), 'Could not upsert test row');
+            Assert::true($this->pdo->commit(), 'Could not commit test row');
         }
+        $this->key = [];
     }
     /**
      * @Then from the ManageRegisteredKey class I should be able to read back: :active :activeAPIMask :keyID :vCode
@@ -106,6 +108,15 @@ class AdminToolsContext implements Context
         $this->mrk->create($keyID, $active, $activeAPIMask, $vCode);
     }
     /**
+     * @When I delete keyID = :keyID in the ManageRegisteredKey class
+     *
+     * @param int $keyID
+     */
+    public function iDeleteKeyIdInTheManageRegisteredKeyClass(int $keyID)
+    {
+        $this->mrk->delete($keyID);
+    }
+    /**
      * @Given I have an initialized instance of PDO connection
      *
      * @throws \InvalidArgumentException
@@ -120,6 +131,13 @@ class AdminToolsContext implements Context
     public function iHaveAnNewInstanceOfTheManageRegisteredKeyClass()
     {
         $this->mrk = new ManageRegisteredKey($this->csq, $this->pdo, $this->yem);
+    }
+    /**
+     * @When I successfully commit the delete in ManageRegisteredKey
+     */
+    public function iSuccessfullyCommitTheDeleteInManageRegisteredKey()
+    {
+        Assert::true($this->mrk->commit(), $this->mrk->getLastErrorString());
     }
     /**
      * @When I successfully commit the new key to the table in ManageRegisteredKey
@@ -141,6 +159,13 @@ class AdminToolsContext implements Context
         $this->key = ['active' => $active, 'activeAPIMask' => $activeAPIMask, 'keyID' => $keyID, 'vCode' => $vCode];
     }
     /**
+     * @Then the isDirty flag should be cleared in ManageRegisteredKey
+     */
+    public function theIsDirtyFlagShouldBeClearedInManageRegisteredKey()
+    {
+        Assert::false($this->mrk->isDirty());
+    }
+    /**
      * @Then the isDirty flag should be set in ManageRegisteredKey
      */
     public function theIsDirtyFlagShouldBeSetInManageRegisteredKey()
@@ -148,8 +173,59 @@ class AdminToolsContext implements Context
         Assert::true($this->mrk->isDirty());
     }
     /**
+     * @Then there should now exist a row in the :tableName table containing: :active :activeAPIMask :keyID :vCode
+     * @Then there should still exist a row in the :tableName table containing: :active :activeAPIMask :keyID :vCode
+     *
+     * @param string $tableName
+     * @param string $active
+     * @param string $activeAPIMask
+     * @param string $keyID
+     * @param string $vCode
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function thereExistsARowInTheTableContaining(
+        string $tableName,
+        string $active,
+        string $activeAPIMask,
+        string $keyID,
+        string $vCode
+    ) {
+        $expected = json_encode([compact('active', 'activeAPIMask', 'keyID', 'vCode')]);
+        $result = json_encode($this->getTableRowsByIndex($tableName,
+            ['active', 'activeAPIMask', 'keyID', 'vCode'],
+            ['keyID' => $keyID]));
+        Assert::same($result, $expected);
+    }
+    /**
+     * @Given there is an existing row in the :tableName table containing: :active :activeAPIMask :keyID :vCode
+     *
+     * @param string $tableName
+     * @param bool   $active
+     * @param int    $activeAPIMask
+     * @param int    $keyID
+     * @param string $vCode
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function thereIsAnExistingRowInTheTableContaining(
+        string $tableName,
+        bool $active,
+        int $activeAPIMask,
+        int $keyID,
+        string $vCode
+    ) {
+        Assert::false($this->pdo->inTransaction(), 'Still have pending transaction');
+        $sql = $this->csq->getUpsert($tableName, ['active', 'activeAPIMask', 'keyID', 'vCode'], 1);
+        Assert::true($this->pdo->beginTransaction(), 'Could not start transaction for test row');
+        $stmt = $this->pdo->prepare($sql);
+        Assert::true($stmt->execute([$active ?: 0, $activeAPIMask, $keyID, $vCode]), 'Could not upsert test row');
+        Assert::true($this->pdo->commit(), 'Could not commit test row');
+    }
+    /**
      * @Given there is not a keyID = :keyID row in the :tableName table
-     * @Then  there should still not be a keyID = :arg1 row in the :arg2 table"
+     * @Then there should still not be a keyID = :keyID row in the :tableName table
+     * @Then there should no longer be a keyID = :keyID row in the :tableName table
      *
      * @param int    $keyID
      * @param string $tableName
@@ -170,12 +246,27 @@ class AdminToolsContext implements Context
     private function getTableRowCount(int $keyID, string $tableName): int
     {
         $sql = sprintf(/** @lang text */
-            'select * from "yapeal-ng"."%s" where "keyID"=%s',
+            'SELECT * FROM "yapeal-ng"."%s" WHERE "keyID"=%s',
             $tableName,
             $keyID);
         $stmt = $this->pdo->query($sql);
+        $stmt->fetchAll();
         $result = $stmt->rowCount();
         $stmt->closeCursor();
+        return $result;
+    }
+    /**
+     * @param string $tableName
+     * @param array  $columnNameList
+     * @param array  $where
+     *
+     * @return array
+     */
+    private function getTableRowsByIndex(string $tableName, array $columnNameList, array $where): array
+    {
+        $sql = $this->csq->getSelect($tableName, $columnNameList, $where);
+        $stmt = $this->pdo->query($sql);
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         return $result;
     }
     /**
@@ -202,11 +293,4 @@ class AdminToolsContext implements Context
      * @var MediatorInterface $yem
      */
     private $yem;
-    /**
-     * @Then there should now exist a row in the :arg1 table containing: :arg2 :arg3 :arg4 :arg5
-     */
-    public function thereShouldNowExistARowInTheTableContaining($arg1, $arg2, $arg3, $arg4, $arg5)
-    {
-        throw new PendingException();
-    }
 }
