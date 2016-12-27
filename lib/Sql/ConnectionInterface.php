d83a6d67a142e4d1483d4017c32eb0c3a5f1982e
Michael Cummings <mgcummings@yahoo.com>
2016-12-27 01:33:41 -0800

    Lots of changes to PDOInterface including rename to ConnectionInterface. Improved the DocBlocks some more as well.

--- lib/Sql/ConnectionInterface.php
+++ lib/Sql/ConnectionInterface.php
@@ -42,9 +42,11 @@ namespace Yapeal\Sql;
  *
  * __NOTE:__
  *     Only include the methods from \PDO that are used by Yapeal-ng in the interface and did so mostly so IDE had
- *     cleaner examples for type hinting etc as the existing stubs are not as clear as could be.
+ *     cleaner examples for type hinting etc as the existing stubs are not as clear as could be plus PHP itself is
+ *     unclear in some cases whither or not some methods take parameters or not and how many. I've tried to clean that
+ *     up with this interface but it should be considered a WIP.
  */
-interface PDOInterface
+interface ConnectionInterface
 {
     /**
      * Initiates a transaction.
@@ -65,11 +67,14 @@ interface PDOInterface
     /**
      * Execute an SQL statement and return the number of affected rows.
      *
-     * @param string $statement
+     * @param string $statement This must be a valid SQL statement for the target database server. This can _not_ be
+     *                          used with anything that returns a result set like SELECT etc. If you need that use
+     *                          <b>\PDO::query</b> or preferably <b>\PDO::prepare</b> and <b>\PDOStatement:execute</b>
+     *                          for better safety and improved repeated query handling.
      *
-     * @return int|false <b>PDO::exec</b> returns the number of rows that were modified or deleted by the SQL statement
-     * you issued. If no rows were affected, <b>PDO::exec</b> returns 0. <b>PDO::exec<b> may return <b>false<b> or emits
-     * <b>\PDOException</b> (depending on error handling).
+     * @return int|false <b>\PDO::exec</b> returns the number of rows that were modified or deleted by the SQL statement
+     * you issued. If no rows were affected, <b>\PDO::exec</b> returns 0. <b>\PDO::exec<b> may return <b>false<b> or
+     * emits <b>\PDOException</b> (depending on error handling).
      * @throws \PDOException
      */
     public function exec(string $statement);
@@ -79,6 +84,12 @@ interface PDOInterface
      * @return bool <b>true</b> if a transaction is currently active, and <b>false</b> if not.
      */
     public function inTransaction(): bool;
+    /**
+     * Flag to indicate if non-wrapper methods of underlying \PDO connection can be used.
+     *
+     * @return bool
+     */
+    public function isExposingPdo(): bool;
     /**
      * Flag to mark if the under-laying PDO driver has already been set to it's most SQL-92 compatible mode or not.
      *
@@ -97,22 +108,28 @@ interface PDOInterface
      *
      * @return \PDOStatement|false If the database server successfully prepares the statement, <b>\PDO::prepare</b>
      * returns a <b>\PDOStatement</b> object. If the database server cannot successfully prepare the statement,
-     * <b>PDO::prepare</b> returns <b>false</b> or emits <b>\PDOException</b> (depending on error handling).
+     * <b>\PDO::prepare</b> returns <b>false</b> or emits <b>\PDOException</b> (depending on error handling).
      * @throws \PDOException
      */
-    public function prepare(string $statement, array $driver_options = []);
+    public function prepare(string $statement, $driver_options = null);
     /**
      * Executes an SQL statement, returning a result set as a PDOStatement object.
      *
-     * @param string $statement The SQL statement to prepare and execute.
-     * @param int    $mode      The fetch mode must be one of the \PDO::FETCH_* constants.
-     * @param mixed  $arguments The second and following parameters are the same as the parameters for
-     *                          \PDOStatement::setFetchMode.
+     * __NOTE:__
+     *     According to PHP this method doesn't accept any parameters including $statement which would make it kind of
+     *     pointless. After take a look at the code for PDO I believe it has to do with a bug in how they try forcing an
+     *     error for the no parameters case but it's some what unclear what's going on really. I'm going to give it
+     *     something cleaner here and deal with it in Connection class with wrapper.
      *
-     * @return \PDOStatement|false <b>PDO::query</b> returns a PDOStatement object, or <b>PDO::exec</b> may return
+     * @param string $statement       The SQL statement to prepare and execute.
+     * @param int    $mode            The fetch mode must be one of the \PDO::FETCH_* constants.
+     * @param mixed  $arguments       The second and following parameters are the same as the parameters for
+     *                                \PDOStatement::setFetchMode.
+     *
+     * @return false|\PDOStatement <b>\PDO::query</b> returns a PDOStatement object, or <b>\PDO::query</b> may return
      * <b>false</b> or emits <b>\PDOException</b> (depending on error handling).
      * @throws \PDOException
-     * @see PDOStatement::setFetchMode For a full description of the second and following parameters.
+     * @see \PDOStatement::setFetchMode For a full description of the second and following parameters.
      */
     public function query(string $statement, int $mode = \PDO::ATTR_DEFAULT_FETCH_MODE, ...$arguments);
     /**
